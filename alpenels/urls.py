416939fe50761d41040005e2b2728b3ecc8c8a2f
Damilola boiyelove <daahrmmieboiye@gmail.com>
2018-12-30 11:04:26 +0100

    changed logged in nav content and debugged logout to exclude admin redirect

--- alpenels/urls.py
+++ alpenels/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', include('core.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='alpenels/forms.html', extra_context={'form_title':'Login'}), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
-    path('admin/', admin.site.urls),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    # path('admin/', admin.site.urls),
 ]
