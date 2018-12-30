c06f7817947ec6107d8c4d47a0d94873f2ac254b
Damilola boiyelove <daahrmmieboiye@gmail.com>
2018-12-30 07:23:02 +0100

    config files

---
+++ alpenels/rod_config.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'alpenels',
+        'USER': 'postalpe',
+        'PASSWORD': 'postalpe',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
